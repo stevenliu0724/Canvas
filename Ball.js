02fdb069772ecac51b465adedc3b90523a731abe
stevenliu0724 <steven.liu0724@gmail.com>
2022-12-02 16:35:33 +0800

    Ball.js

--- Ball.js
+++ Ball.js
@@ -1,6 +1,54 @@
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("CVS");
 const c = canvas.getContext("2d");
+const particleArray = [];
 
-const canvas.width = window.innerWidth;
-const canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
+class Particle {
+    constructor(){
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 10 + 1;
+        this.speedx = Math.random() * 3 - 1.5;
+        this.speedy = Math.random() * 3 - 1.5;
+    };
+    
+    update(){
+        this.x += this.speedx;
+        this.y += this.speedy;
+    };
+
+    draw(){
+        c.beginPath();
+        c.arc(this.x, this.y, this.size, 0, Math.PI *2, false);
+        c.fillStyle = "rgba(255, 0, 0, 0.5)";
+        c.strokeStyle = "rgba(255, 0, 0, 0.6)";
+        c.fill();
+        c.stroke();
+    };
+
+};
+
+function init(){
+    for (let i = 0; i < 100; i++){
+        particleArray.push(new Particle());
+    };
+};
+init();
+
+function handleparticle(){
+    for (let i = 0; i < particleArray.length; i++){
+        particleArray[i].update();
+        particleArray[i].draw();
+
+    };
+};
+
+function animate(){
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    handleparticle();
+    requestAnimationFrame(animate);
+};
+
+animate();
